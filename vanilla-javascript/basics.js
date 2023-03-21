0c3819320ddc9fd18f09d1ad8d685b92ee42873c
Rokas <94966788+Rokiis1@users.noreply.github.com>
2023-03-21 15:14:37 +0000

    basics js

---
+++ vanilla-javascript/basics.js
@@ -0,0 +1,72 @@
+// Variables
+// In JavaScript, variables are used to store data values that can be later referenced or manipulated. 
+// Variables can be declared using the var, let, or const keywords. 
+// var is used to declare variables that are function-scoped, while let and const are used to declare variables that are block-scoped. 
+// Here's an example:
+
+// declaring variables using var
+var x = 5;
+var y = "Hello World!";
+
+// declaring variables using let
+let z = true;
+
+// declaring variables using const
+const PI = 3.14;
+// Data Types
+// Number: Represents numeric values, such as 1, 2.5, or -3.
+// String: Represents text values, enclosed in single or double quotes, such as "hello" or 'world'.
+// Boolean: Represents a logical value, either true or false.
+// Null: Represents the intentional absence of any object value.
+// Undefined: Represents a variable that has been declared but has not been assigned a value.
+// Object: Represents a collection of properties and values, such as {name: "John", age: 30}.
+// Array: Represents a list of values, enclosed in square brackets, such as [1, 2, 3].
+
+// declaring numbers
+let num1 = 10;
+let num2 = 3.14;
+
+// declaring strings
+let str1 = "Hello";
+let str2 = 'World';
+
+// declaring booleans
+let bool1 = true;
+let bool2 = false;
+
+// declaring objects
+let person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// declaring arrays
+let arr1 = [1, 2, 3, 4];
+let arr2 = ["apple", "banana", "orange"];
+
+// declaring null and undefined values
+let var1 = null;
+let var2 = undefined;
+
+// Operators:
+// Operators in JavaScript are used to perform operations on values and variables. 
+// There are several types of operators, including arithmetic, comparison, logical, and assignment operators.
+
+// Arithmetic operators
+let sum = 2 + 2;
+let difference = 10 - 5;
+let product = 3 * 4;
+let quotient = 15 / 3;
+
+// Comparison operators
+let isEqual = 3 == 3;
+let isNotEqual = 5 != 10;
+let isGreaterThan = 10 > 5;
+let isLessThan = 3 < 7;
+
+// Logical operators
+let and = true && false;
+let or = true || false;
+let not = !true;
+
